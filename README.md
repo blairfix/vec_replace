3b79ce882afc4844931ad4dfccf0acb4454595eb
blairfix <blairfix@gmail.com>
2022-05-31 14:28:13 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,48 @@
+# vec_replace
+
+`vec_replace` is an R function for finding and replacing strings withing a vector.
+
+
+### Inputs
+
+The `vec_replace` function has three inputs:
+
+
+* `` = a vector of string in which to search and replace
+* `find` =  the strings to look for
+* `replace` = the strings to replace
+
+
+
+### Output
+`vec_replace` returns the vector `x` with the appropriate substitutions. 
+
+
+### Example
+
+
+```R
+library(Rcpp)
+sourceCpp("vec_replace.cpp")
+
+x = c("Bob", "Harry", "Sally", "Angela", "David")
+
+find = c("Bob", "Angela")
+replace = c("test1", "test2")
+
+x_replace = vec_replace(x, find, replace)
+
+
+```
+
+
+
+### Installation
+To use `vec_replace`, install the following R packages:
+ * [Rcpp](https://cran.r-vec_replace.org/web/packages/Rcpp/index.html) 
+
+Put the source code (`vec_replace.cpp`) in the directory of your R script. Then source it with the command `sourceCpp('vec_replace.cpp')`.
+
+
+
+
---
+++ vec_replace.cpp
@@ -0,0 +1,103 @@
+#include <Rcpp.h>
+#include <string>
+#include <vector>
+#include <numeric>      
+#include <algorithm> 
+
+using namespace Rcpp;
+
+// [[Rcpp::plugins(cpp11)]]
+// [[Rcpp::export]]
+
+CharacterVector vec_replace( 
+			    CharacterVector x,
+			    CharacterVector find,
+			    CharacterVector replace
+			    )
+
+{
+
+    // sort x and keep sorted ids
+    ///-----------------------------------------------------
+
+    // make original id vector
+    int n_x = x.size();
+    IntegerVector id_x = seq(0, n_x - 1);
+
+    // get index of sorted ids
+     std::iota( id_x.begin(), id_x.end(), 0 ); 
+     std::sort( id_x.begin(), id_x.end(), [&](int i,int j){return x[i] < x[j]; } );
+
+    // sort x 
+    x = x[id_x];
+
+
+    // sort find and replace in same order
+    ///-----------------------------------------------------
+
+    // make original id vector
+    int n_find = find.size();
+    IntegerVector id_find = seq(0, n_find - 1);
+
+    // get index of sorted ids
+     std::iota( id_find.begin(), id_find.end(), 0); 
+     std::sort( id_find.begin(), id_find.end(), [&](int i,int j){return find[i] < find[j]; } );
+
+    // sort find and replace
+    find = find[id_find];
+    replace = replace[id_find];
+
+
+
+    // loop over x_sorted and find / replace
+    int find_counter = 0;
+
+    for( int i = 0; i < n_x; i++ ){
+
+	// if find counter not at end
+	// and x matches find at current counter
+	//  then replace x with replace
+	    
+
+	if( find_counter < n_find && x[i] == find[ find_counter ] ){
+	    
+	    // save old element
+	    String x_old = x[i];
+	    
+	    // replace element
+	    x[i] =  replace[ find_counter ];
+
+	    // if next element still in x
+	    // test if next element of x equals current element
+	    // if true, then advance find counter
+
+	    if( i + 1 < n_x ){
+
+		if( x_old != x[i+1] ){
+
+		    find_counter++;
+
+		}
+	    }
+	}
+    }
+
+
+    // return x to original order
+    //-----------------------------------------------------
+
+
+    // make final id vector
+    IntegerVector id_final = seq(0, n_x - 1);
+
+    // get index of sorted ids
+     std::iota( id_final.begin(), id_final.end(), 0); 
+     std::sort( id_final.begin(), id_final.end(), [&](int i,int j){return id_x[i] < id_x[j]; } );
+
+    // sort in original order
+     x = x[id_final];
+
+
+    return x;
+
+}
